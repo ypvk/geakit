6b89a04e56de369b75d3b7f7902c2a4d95d5470a
Yu Ping <ypseu.2007@163.com>
2012-05-24 12:19:26 +0800

    changed the api

--- geakit/src/api/gaccountapi.cpp
+++ geakit/src/api/gaccountapi.cpp
@@ -5,8 +5,8 @@
 #include <QDebug>
 #include "gaccountapi.h"
 
-const QString url = "https://api.github.com/user";//api url
-const int timeToRun = 1000;//time to connect or run the api;
+const QString url = "https://api.github.com/users/ypvk";//api url
+const int timeToRun = 11000;//time to connect or run the api;
 
 GAccountAPI::GAccountAPI(QNetworkAccessManager* manager)
 {
@@ -16,12 +16,24 @@ GAccountAPI::GAccountAPI(QNetworkAccessManager* manager)
   m_timeout = new QTimer(this);
 
   connect(m_timeout, SIGNAL(timeout()), this, SLOT(onTimeout()));
-  connect(m_manager, SIGNAL(finished(QNetWorkReply*)), this, SLOT(parseFinished(QNetWorkReply*)));
+  connect(m_manager, SIGNAL(finished(QNetworkReply*)), this, SLOT(parseFinished(QNetworkReply*)));
     
 }
 void GAccountAPI::parseFinished(QNetworkReply* reply) {
-    
+  QNetworkReply::NetworkError err = reply->error();
+  switch (err) {
+    case QNetworkReply::NoError:
+      {
+        qDebug() << reply->readAll();
+     //   qDebug() << reply->rawHeaderPairs();
+        break;
+      }
+    default:
+      qDebug() << "error is " << err;
 
+  }  
+
+  exit(0);
 }
 
 void GAccountAPI::startConnect() {
@@ -34,6 +46,7 @@ void GAccountAPI::onTimeout() {
   //todo something if the time is out
   m_manager->setNetworkAccessible(QNetworkAccessManager::NotAccessible);
   qDebug() << "can't get api message";
+  exit(1);
 }
 void GAccountAPI::setNetManager(QNetworkAccessManager* manager)
 {
--- geakit/tests/test_API.cpp
+++ geakit/tests/test_API.cpp
@@ -1,8 +1,11 @@
 #include <api/gaccountapi.h>
+#include <QNetworkAccessManager>
 #include <QApplication>
 
 int main (int argc, char* argv[]) {
   QApplication app(argc, argv);
-  GAccountAPI api;
+  QNetworkAccessManager m_network;
+  GAccountAPI api(&m_network);
+  api.startConnect();
   return app.exec();
 } 
